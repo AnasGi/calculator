c7b231d8c04dc3c04dc828499f84290ae3a5e454
AB <anassboussalem8@gmail.com>
2023-12-03 21:02:37 +0100

    v3

--- src/project.js
+++ src/project.js
@@ -9,14 +9,14 @@ export default function Proj(){
 
     let [nb , setNb] = useState("0")
     const [nb2 , setNb2] = useState("0")
-    const [res , setRes] = useState(0)
+    const [res , setRes] = useState(0.0)
     const [opr , setOpr] = useState("")
     const [b,setB] = useState(false)
     
     return(
         <div className="cont">
             <div>
-                <input type="text" className="show" value={(b===false)?parseFloat(nb):parseFloat(nb)+opr+parseInt(nb2)} disabled />
+                <input type="text" className="show" value={(b===false)?parseFloat(nb):parseFloat(nb)+opr+parseFloat(nb2)} disabled />
                 {/* (b===false)?parseFloat(nb) means that if we dont click on any operator the operations input will show nb only wich is the first number */}
             </div>
 
@@ -34,16 +34,22 @@ export default function Proj(){
                 <input type="text" id="res" readOnly value={"="} onClick={()=>{
 
                     if (opr === "+"){
-                        setRes(parseInt(parseInt(nb) + parseInt(nb2)))
+                        setRes(parseFloat(parseFloat(nb) + parseFloat(nb2)))
                     }
                     else if (opr === '-'){
-                        setRes(parseInt(parseInt(nb) - parseInt(nb2)))
+                        setRes(parseFloat(parseFloat(nb) - parseFloat(nb2)))
                     }
                     else if (opr === '*'){
-                        setRes(parseInt(parseInt(nb) * parseInt(nb2)))
+                        setRes(parseFloat(parseFloat(nb) * parseFloat(nb2)))
                     }
                     else{
-                        nb2!=="0"?setRes(parseInt(parseInt(nb) / parseInt(nb2))):setRes('Impossible');setTimeout(()=>window.location.reload(false),1000)
+                        if(nb2 !== 0){
+                            setRes(parseFloat(parseFloat(nb) / parseFloat(nb2)))
+                        }
+                        else{
+                            setRes('Impossible');
+                            setTimeout(()=>window.location.reload(false),1000)
+                        }
                     }
                     setNb2('0') //to initialise nbr2 for the next operation
                     setNb('0') //to bring the oparations input to 0
@@ -53,7 +59,7 @@ export default function Proj(){
                 
             </div>
             <div>
-                <input type="text" className="show" disabled value={res} />
+                <input type="text" className="show" disabled value={parseFloat(res)} />
             </div>
             <div className="reset_cont">
                 <button onClick={()=>{window.location.reload(false)}}>Reset</button>
